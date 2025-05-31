4f11e1f7ca4d62cb29fbc935ad5631e628c0fb7e
HaoHV2k5 <magicmath2k5@gmail.com>
2025-05-31 11:43:13 +0700

    init

---
+++ src/main/java/com/vn/LaptopShop/service/UserService.java
@@ -0,0 +1,11 @@
+package com.vn.LaptopShop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String userHelloService() {
+        return "hello from user service";
+    }
+}
